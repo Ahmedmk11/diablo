309f0607ea771cf655218f7b1e1a1c9775827f45
ibraE1 <ibrahimelwany@hotmail.com>
2024-12-02 16:58:38 +0200

    Added HUD and Minimap

--- Assets/Scripts Yasser/Movement.cs
+++ Assets/Scripts Yasser/Movement.cs
@@ -22,7 +22,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
         {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(ray, out hit))
